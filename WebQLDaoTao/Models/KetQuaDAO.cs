31da169d41e125b0633784b5882b7230a5c6b2e8
TLUC D <ltu2733@gmail.com>
2025-03-07 10:34:39 +0700

    QLDiemThi, Update, Delete

--- WebQLDaoTao/Models/KetQuaDAO.cs
+++ WebQLDaoTao/Models/KetQuaDAO.cs
@@ -15,7 +15,8 @@ namespace WebQLDaoTao.Models
 
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString);
             conn.Open();
-            SqlCommand cmd = new SqlCommand("select * from ketqua where mamh=@mamh", conn);
+            SqlCommand cmd = new SqlCommand("select ketqua.*, hosv, tensv from " +
+                "ketqua inner join sinhvien on ketqua.masv = sinhvien.masv where mamh=@mamh", conn);
               
             cmd.Parameters.AddWithValue("@mamh", mamh);
             SqlDataReader rd = cmd.ExecuteReader();
@@ -26,7 +27,8 @@ namespace WebQLDaoTao.Models
                     Id = int.Parse(rd["id"].ToString()),
                     MaSV = rd["MaSV"].ToString(),
                     MaMH = rd["mamh"].ToString(),
-                   
+                    HoTenSV = rd["hosv"].ToString() + " " + rd["tensv"].ToString()
+
                 };
                 if (!string.IsNullOrEmpty(rd["diem"].ToString()))
                 {
@@ -37,6 +39,23 @@ namespace WebQLDaoTao.Models
             }
 
             return ds;
+        }
+        public int Update (int id, float diem)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("update ketqua set diem=@diem where id=@id", conn);
+            cmd.Parameters.AddWithValue("@diem", diem);
+            cmd.Parameters.AddWithValue("@id", id);
+            return cmd.ExecuteNonQuery();
+        }
+        public int Delete(int id)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("delete from ketqua where id=@id", conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            return cmd.ExecuteNonQuery();
         }
     }
 }
